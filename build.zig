f3f77bea61ae53fcacbf957716e1d02667d95772
SmolNero <smolnero@gmail.com>
2025-01-02 18:18:11 -0700

    olo

---
+++ build.zig
@@ -0,0 +1,6 @@
+// Standard optimization options allow the person running 'zig build' to select
+// between Debug, ReleaseSafe, releaseFast, and ReleaseSmall
+
+const optimize = b.standarOptimizationOption(.{});
+
+const exe = b.addExecutable(.{ .name = "villin" });
