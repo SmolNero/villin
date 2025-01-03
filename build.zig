ad9e3dbd242483d365947f28a29a9f0082436774
SMOL <smolnero@gmail.com>
2025-01-02 20:43:16 -0700

    olo

--- build.zig
+++ build.zig
@@ -1,28 +1,39 @@
 const std = @import("std");
 
-
 pub fn build(b: *std.Build) void {
+
+    // Standard target options allows the person running 'zig build' to choose
+    // what target to build for. Here we do not override the defaults
+    
+    const target = b.standardTargetOptions(.{});
 
     // Standard optimization options allow the person running 'zig build' to select
     // between Debug, ReleaseSafe, releaseFast, and ReleaseSmall
-    const optimize = b.standarOptimizationOption(.{});
     
+    const optimize = b.standardOptimizeOptions(.{});
+
     const exe = b.addExecutable(.{
         .name = "villin",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" steps
-    b.installArtifact(exe);
     
+    b.installArtifact(exe);
+
     // Creates a step for unit testing
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{.path = "src/main.zig"},
-        .taget = target,
-        .optimize = optimize
+    
+    const unit_tests = b.addTest(.{ 
+    .root_source_file = .{ .path = "src/main.zig" }, 
+    .taget = target, 
+    .optimize = optimize,
+
     });
+
+
+
 
 }
