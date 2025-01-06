8762fdcc6129c523a69fef7f39c44e47d007795a
SmolNero <smolnero@gmail.com>
2025-01-05 22:01:39 -0700

    olo

--- src/compression/metrics.zig
+++ src/compression/metrics.zig
@@ -5,10 +5,11 @@ pub const CompressionMetrics = struct {
     allocator: std.mem.Alllcator,
 
     // Core metrics
+    total_bytes_in: u64 = 0,
+    total_bytes_out: u64 = 0,
+    patterns_detected: u64 = 0,
+    processing_time_ns: u64 = 0,
 
-
-
-
-
-
-}
+    // Pattern metrics
+    patter_hits: std.AutoHasMap(u64, u64),
+};
