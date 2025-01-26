984834fae3b347c266066b190e7ab0a5e22f0618
SMOL <smolnero@gmail.com>
2025-01-26 10:59:54 -0700

    core

--- src/compression/villin_core.zig
+++ src/compression/villin_core.zig
@@ -2,5 +2,15 @@ cont std = @import("std");
 
 pub const CompressEngine = struct {
 	allocator: std.mem.Allocator,
+	config: ComressConfig,
+	metrics: ?*Metrics,
+
+	pub const CompressConfig = struct {
+		
+	}
+
+
+
+
 }
 
